8a44ee5cf7699fc889d1e49fb743d53981434882
Andrey <andrey@mac-admin.local>
2016-02-20 17:45:53 +0300

    Initial Commit

---
+++ ObjCPatterns/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjCPatterns
+//
+//  Created by Andrey on 20.02.16.
+//  Copyright © 2016 Andrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjCPatterns/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjCPatterns
+//
+//  Created by Andrey on 20.02.16.
+//  Copyright © 2016 Andrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
